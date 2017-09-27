052555d497f73cb363d6b78b39223a693fa989bc
David Schlachter <david+gh@schlachter.ca>
2017-09-27 13:41:24 -0400

    Add FlowSpecies class

---
+++ FlowSpecies.java
@@ -0,0 +1,14 @@
+/**
+ * Add flow data to Species object
+ */
+public class FlowSpecies extends Species {
+  
+  private overallMoleFraction = 0;
+  private liquidMoleFraction = 0;
+  private gasMoleFraction = 0;
+  
+  public boolean FlowSpecies() {
+    return true;
+  }
+  
+}
