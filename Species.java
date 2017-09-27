1a0379072547c09b8f17a1890941b7defb07444a
David Schlachter <david+gh@schlachter.ca>
2017-09-27 15:04:38 -0400

    Add getter, setters, and tests to Species class

--- Species.java
+++ Species.java
@@ -6,25 +6,81 @@
 public class Species {
   
   // Heat capacity constants
-  private double c1 = 0;
-  private double c2 = 0;
-  private double c3 = 0;
-  private double c4 = 0;
+  private double heatCapacityA = 0;
+  private double heatCapacityB = 0;
+  private double heatCapacityC = 0;
+  private double heatCapacityD = 0;
   
   // Antoine coefficients
-  private double a = 0;
-  private double b = 0;
-  private double c = 0;
+  private double antoineA = 0;
+  private double antoineB = 0;
+  private double antoineC = 0;
   
   // Heat of vapourization
-  private heatOfVapourization = 0;
+  private double heatOfVapourization = 0;
   
   // Activity coefficient (default value is 1 for an ideal solution)
-  private activityCoefficient = 1;
+  private double activityCoefficient = 1;
   
   // default constructor method
   public boolean Species() {
     return true;
+  }
+  
+  
+  // Setters
+  public void setHeatCapacityConstants(double heatCapacityA, double heatCapacityB, double heatCapacityC, double heatCapacityD) {
+    this.heatCapacityA = heatCapacityA;
+    this.heatCapacityB = heatCapacityB;
+    this.heatCapacityC = heatCapacityC;
+    this.heatCapacityD = heatCapacityD;
+  }
+  
+  public void setAntoineConstants(double antoineA, double antoineB, double antoineC) {
+    this.antoineA = antoineA;
+    this.antoineB = antoineB;
+    this.antoineC = antoineC;
+  }
+  
+  public void setHeatOfVapourization(double heatOfVapourization) {
+    this.heatOfVapourization = heatOfVapourization;
+  }
+  
+  public boolean setActivityCoefficient(double activityCoefficient) {
+    if (activityCoefficient > 0.0 && activityCoefficient <= 1.0) {
+      this.activityCoefficient = activityCoefficient;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // Getters
+  public double[] getHeatCapacityConstants() {
+    double[] heatCapacityConstants = new double[4];
+    heatCapacityConstants[0] = this.heatCapacityA;
+    heatCapacityConstants[1] = this.heatCapacityB;
+    heatCapacityConstants[2] = this.heatCapacityC;
+    heatCapacityConstants[3] = this.heatCapacityD;
+    
+    return heatCapacityConstants;
+  }
+  
+  public double[] getAntoineConstants() {
+    double[] antoineConstants = new double[3];
+    antoineConstants[0] = this.antoineA;
+    antoineConstants[1] = this.antoineB;
+    antoineConstants[2] = this.antoineC;
+    
+    return antoineConstants;
+  }
+  
+  public double getHeatOfVapourization() {
+    return this.heatOfVapourization;
+  }
+  
+  public double getActivityCoefficient() {
+    return this.activityCoefficient;
   }
   
 }
--- Test_Species.java
+++ Test_Species.java
@@ -8,12 +8,74 @@ import junit.framework.TestCase;
 public class Test_Species extends TestCase {
   
   /**
-   * A test method.
-   * (Replace "X" with a name describing the test.  You may write as
-   * many "testSomething" methods in this class as you wish, and each
-   * one will be called when running JUnit over this class.)
+   * Test the getter and setter for the heat capacity constants for a species
    */
-  public void testX() {
+  public void testHeatCapacityConstants() {
+    
+    Species testSpecies = new Species();
+    
+    double heatCapacityA = 1.0;
+    double heatCapacityB = 1.0;
+    double heatCapacityC = 1.0;
+    double heatCapacityD = 1.0;
+    
+    testSpecies.setHeatCapacityConstants(heatCapacityA, heatCapacityB, heatCapacityC, heatCapacityD);
+    
+    double[] heatCapacityConstants = testSpecies.getHeatCapacityConstants();
+    
+    assertEquals("Species.set/getHeatCapacityConstants[0]", heatCapacityA, heatCapacityConstants[0]);
+    assertEquals("Species.set/getHeatCapacityConstants[1]", heatCapacityB, heatCapacityConstants[1]);
+    assertEquals("Species.set/getHeatCapacityConstants[2]", heatCapacityC, heatCapacityConstants[2]);
+    assertEquals("Species.set/getHeatCapacityConstants[3]", heatCapacityD, heatCapacityConstants[3]);
+    
+  }
+  
+  /**
+   * Test the getter and setter for the Antoine equation coefficients for a species
+   */
+  public void testAntoineConstants() {
+    
+    Species testSpecies = new Species();
+    
+    double antoineA = 1.0;
+    double antoineB = 1.0;
+    double antoineC = 1.0;
+    
+    testSpecies.setAntoineConstants(antoineA, antoineB, antoineC);
+    
+    double[] antoineConstants = testSpecies.getAntoineConstants();
+    
+    assertEquals("Species.set/getAntoineConstants[0]", antoineA, antoineConstants[0]);
+    assertEquals("Species.set/getAntoineConstants[1]", antoineB, antoineConstants[1]);
+    assertEquals("Species.set/getAntoineConstants[2]", antoineC, antoineConstants[2]);
+
+    
+  }
+  
+  /**
+   * Test the getter and setter for the heat of vapourization
+   */
+  public void testHeatOfVapourization() {
+    Species testSpecies = new Species();
+    
+    double heatOfVapourization = 1.0;
+    testSpecies.setHeatOfVapourization(heatOfVapourization);
+    
+    assertEquals("Species.set/getHeatOfVapourization", heatOfVapourization, testSpecies.getHeatOfVapourization());
+    
+  }
+  
+  
+  /**
+   * Test the getter and setter for the activity coefficient
+   */
+  public void testActivityCoefficient() {
+    Species testSpecies = new Species();
+    
+    double activityCoefficient = 0.5;
+    assertEquals("Species.setActivityCoefficient(0)", true, testSpecies.setActivityCoefficient(activityCoefficient));
+    assertEquals("Species.getActivityCoefficient()", activityCoefficient, testSpecies.getActivityCoefficient());
+    
   }
   
 }
