bc74385cba137ae6ff56399543f1ba2ec9bdc519
David Schlachter <david+gh@schlachter.ca>
2017-09-27 14:03:07 -0400

    Add blank unit test for Species

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   
     public static void main(String[] args) {
-      System.out.println("Testing this! YAY :)");
+      
+      // Some unit tests for the methods that we have so far!
+      
+      
     }
     
 }
