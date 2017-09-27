e04ec7602e40020806ea96ce9f7564b800e5aa9e
David Schlachter <david+gh@schlachter.ca>
2017-09-27 15:03:14 -0400

    Fix synthax errors

--- FlowSpecies.java
+++ FlowSpecies.java
@@ -3,9 +3,9 @@
  */
 public class FlowSpecies extends Species {
   
-  private overallMoleFraction = 0;
-  private liquidMoleFraction = 0;
-  private gasMoleFraction = 0;
+  private double overallMoleFraction = 0;
+  private double liquidMoleFraction = 0;
+  private double gasMoleFraction = 0;
   
   public boolean FlowSpecies() {
     return true;
--- FlowStream.java
+++ FlowStream.java
@@ -1,10 +1,14 @@
 /**
  * FlowStream
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlowStream {
   
   // The species with compositions in the flow stream
-  private List<FlowSpecies> flowSpecies = newArrayList<FlowSpecies>();
+  private List<FlowSpecies> flowSpecies = new ArrayList<FlowSpecies>();
   
   // Stream properties
   private double molarFlowRate;
