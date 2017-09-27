c5cbffde4b911de5f453c814f36f8393e5fcd660
David Schlachter <david+gh@schlachter.ca>
2017-09-27 13:46:23 -0400

    Add FlowStream class

---
+++ FlowStream.java
@@ -0,0 +1,20 @@
+/**
+ * FlowStream
+ */
+public class FlowStream {
+  
+  // The species with compositions in the flow stream
+  private List<FlowSpecies> flowSpecies = newArrayList<FlowSpecies>();
+  
+  // Stream properties
+  private double molarFlowRate;
+  private double temperature;
+  private double pressure;
+  
+  
+  // Default constructor
+  public boolean FlowStream() {
+    return true;
+  }
+  
+}
