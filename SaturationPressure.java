46b1a5941e40b24007ce45c9d22a961322d4ba02
David Schlachter <david+gh@schlachter.ca>
2017-09-29 16:29:00 -0400

    Add saturation pressure calculation class, and test

---
+++ SaturationPressure.java
@@ -0,0 +1,11 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class SaturationPressure {
+  
+  public static double calc(Species species, double temperature) {
+    double[] antoineConstants = species.getAntoineConstants();
+    return(Math.pow(10.0, antoineConstants[0] - (antoineConstants[1]/(antoineConstants[2] + temperature))));
+  }
+  
+}
---
+++ Test_SaturationPressure.java
@@ -0,0 +1,29 @@
+import junit.framework.TestCase;
+
+/**
+ * A JUnit test case class.
+ * Every method starting with the word "test" will be called when running
+ * the test with JUnit.
+ */
+public class Test_SaturationPressure extends TestCase {
+  
+  // Test the static saturation pressure calculation method
+  public void testSaturationPressure() {
+    
+    Species testSpecies = new Species();
+    
+    // Antoine coefficients for water between 0-100 ºC, with units in Pa, K
+    // source: Wikipedia  :)
+    testSpecies.setAntoineConstants(10.19621302, 1730.63, -39.724);
+    // Temperature in K for the saturation pressure calculation
+    double temperature = 298.15;
+    
+    double saturationPressure = SaturationPressure.calc(testSpecies, temperature);
+    
+    // Account for rounding errors in floating point arithmetic
+    assertEquals("SaturationPressure.calc(water, 298.15)", true, (saturationPressure > 3157.9  && saturationPressure < 3158.0));
+    assertEquals("SaturationPressure.calc(water, 298.15)", false, (saturationPressure < 3157.9 || saturationPressure > 3158.0));
+    
+  }
+  
+}
