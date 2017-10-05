536046521958e43feb8b01be07868af8c316eeb4
David Schlachter <david+gh@schlachter.ca>
2017-10-05 18:08:10 -0400

    Reformatting for R&K style

--- Enthalpy.java
+++ Enthalpy.java
@@ -9,9 +9,7 @@ public class Enthalpy implements Function {
     
     this.inlet = inlet;
     this.outlet = outlet;
-    
-    
-    
+
   }
   
   public double testFunction(double testTemp){
@@ -24,39 +22,33 @@ public class Enthalpy implements Function {
     
     System.out.println("Inside: " + this.inlet.getTemperature() + " " + this.outlet.getTemperature());
     
-    if(this.inlet.getTemperature() <0.01){
-    initialTemperature = testTemp;
-    finalTemperature = this.outlet.getTemperature();
-    } 
-    
-    else if(this.outlet.getTemperature() < 0.01){
+    if (this.inlet.getTemperature() < 0.01) {
+      initialTemperature = testTemp;
+      finalTemperature = this.outlet.getTemperature();
+    } else if (this.outlet.getTemperature() < 0.01) {
       finalTemperature = testTemp;
       initialTemperature = this.inlet.getTemperature();
-    } else{
-    System.out.println("Failure to initialize temperatures to real values."); 
-    System.exit(1);
+    } else {
+      System.out.println("Failure to initialize temperatures to real values."); 
+      System.exit(1);
     }
-    if(this.inlet.getNumberOfSpecies() != this.outlet.getNumberOfSpecies()){
-    System.out.println("Inlet and outlet streams do not have same number of species."); 
-    System.exit(1);
+    
+    if (this.inlet.getNumberOfSpecies() != this.outlet.getNumberOfSpecies()) {
+      System.out.println("Inlet and outlet streams do not have same number of species."); 
+      System.exit(1);
     }
     
      for (i = 0; i < this.outlet.getNumberOfSpecies(); i++) {
-     
       overallMoleFraction = this.outlet.getFlowSpecies().get(i).getOverallMoleFraction();
       vapourMoleFraction = this.outlet.getFlowSpecies().get(i).getVapourMoleFraction();
       heatOfVapourization = this.outlet.getFlowSpecies().get(i).getHeatOfVapourization();
       
       result = result + vapourMoleFraction * (heatOfVapourization) + 
         overallMoleFraction * HeatCapacity.integrate(this.outlet.getFlowSpecies().get(i), initialTemperature, finalTemperature);
-
     }
-
 
      return result;
 
-  
   }
-  
-  
+
 }
