dddae88b30c21ffb5697cc7ba891dc22da6a8de7
David Schlachter <david+gh@schlachter.ca>
2017-10-11 15:17:29 -0400

    Add ability to add and remove species in ConsoleUI

--- ConsoleUI.java
+++ ConsoleUI.java
@@ -14,44 +14,96 @@ public class ConsoleUI {
   }
   
   // Run the ConsoleUI
-  public void run() {
-    
-    Scanner scan = new Scanner(System.in);
+  public void run(Scanner scan) {
     
     int i;
     char choice;
     
+    System.out.println("\nSelect species for the simulation:");
+    
     while (0 == 0) {
-      
-      System.out.println("\nSelect species for the simulation:");
-      System.out.println("Current species selected:");
+     
+      System.out.println("Current species selected:\n");
       if (this.theseSpecies.size() == 0) {
-        System.out.println("[ No species selected ]\n");
+        System.out.println("  [ No species selected ]\n");
       } else {
         for (i = 0; i < this.theseSpecies.size(); i++) {
-          System.out.println(i + " " + this.theseSpecies.get(i).getSpeciesName());
+          System.out.println("  " + this.theseSpecies.get(i).getSpeciesName());
         }
+        System.out.print("\n");
       }
       System.out.println("Select an action: [a]dd species   [r]emove species   [d]one\n");
 
       choice = scan.nextLine().charAt(0);
       
-      if (choice == 'a') this.addSpecies();
-      if (choice == 'r') this.removeSpecies();
+      if (choice == 'a') this.addSpecies(new Scanner(System.in));
+      if (choice == 'r') this.removeSpecies(new Scanner(System.in));
       if (choice == 'd') break;
     }
+    
+    FlowStream inletStream  = new FlowStream();
+    FlowStream outletStream = new FlowStream();
+    inletStream.setFlowSpecies(this.theseSpecies);
+    outletStream.setFlowSpecies(this.theseSpecies);
     
     scan.close();
   
   }
   
-  private void addSpecies() {
-    System.out.println("Adding a species");
+  private void addSpecies(Scanner scan) {
+    int i, choice;
+    
+    while (0 == 0) {
+      System.out.println("\nPlease select a species to add from the list, or specify a new species:\n");
+      
+      for (i = 0; i < this.presetSpecies.size(); i++) {
+        System.out.println("  " + i + " " + this.presetSpecies.get(i).getSpeciesName());
+      }
+      System.out.println("------------------------");
+      System.out.println("  " + i + " New species...\n");
+      
+      choice = scan.nextInt();
+      
+      if (choice >= 0 && choice <= this.presetSpecies.size()) break;
+      
+    }
+    
+    if (choice != this.presetSpecies.size()) {
+      this.theseSpecies.add(this.presetSpecies.get(choice));
+    } else {
+      this.addCustomSpecies(new Scanner(System.in));
+    }
+    
+
+    scan.close();
   };
   
-  private void removeSpecies() {
-    System.out.println("Removing a species");
+  private void removeSpecies(Scanner scan) {
+    int i, choice;
+    
+    while (0 == 0) {
+      System.out.println("\nPlease select a species to remove from the list:\n");
+      
+      for (i = 0; i < this.theseSpecies.size(); i++) {
+        System.out.println("  " + i + " " + this.theseSpecies.get(i).getSpeciesName());
+      }
+      System.out.print("\n");
+      
+      choice = scan.nextInt();
+      
+      if (choice >= 0 && choice < this.presetSpecies.size()) break;
+      
+    }
+    
+    this.theseSpecies.remove(choice);
+    
+
+    scan.close();
   };
+  
+  private void addCustomSpecies(Scanner scan) {
+    System.out.println("\nAdding a custom species\n");
+  }
   
   
 }
