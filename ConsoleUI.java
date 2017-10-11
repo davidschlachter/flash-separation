58b9193de11793fed64e9fc23ac216417ba0017b
David Schlachter <david+gh@schlachter.ca>
2017-10-11 19:47:33 -0400

    Get stream properties with ConsoleUI

--- ConsoleUI.java
+++ ConsoleUI.java
@@ -15,14 +15,18 @@ public class ConsoleUI {
   }
   
   // Run the ConsoleUI
-  public void run(Scanner scan, PrintWriter output) {
+  public boolean run(Scanner scan, PrintWriter output) {
     
     int i;
     char choice;
+    double nextDouble;
+    String nextString;
     boolean firstRun = true;
     
+    //
+    // Get the list of species that will be simulated
+    //
     output.println("\nSelect species for the simulation:");
-    
     while (true) {
      
       output.println("Current species selected:\n");
@@ -45,17 +49,220 @@ public class ConsoleUI {
       
       if (choice == 'a') this.addSpecies(scan, output);
       if (choice == 'r') this.removeSpecies(scan, output);
-      if (choice == 'q') return;  // Exit option for testing
+      if (choice == 'q') return true;  // Exit option for testing
       if (choice == 'd') break;
     }
     
     FlowStream inletStream  = new FlowStream();
     FlowStream outletStream = new FlowStream();
-    inletStream.setFlowSpecies(this.theseSpecies);
-    outletStream.setFlowSpecies(this.theseSpecies);
+    for (i = 0; i < this.theseSpecies.size(); i++) {
+      inletStream.addFlowSpecies(new FlowSpecies(this.theseSpecies.get(i)));
+      outletStream.addFlowSpecies(new FlowSpecies(this.theseSpecies.get(i)));
+    }
+    
+    //
+    // Get the mole fractions of these species in the inlet stream
+    //
+    output.println("\nFor each species in the inlet stream, please set the liquid mole fractions: \n");
+    double moleFraction = -1.0, moleFractionSum = 0.0;
+    while (true) {
+      for (i = 0; i < inletStream.getFlowSpecies().size(); i++) {
+        while (true) {
+          output.println("  Mole fraction of " + inletStream.getFlowSpecies().get(i).getSpeciesName() + ": ");
+          moleFraction = scan.nextDouble();
+          if (moleFraction >= 0.0 && moleFraction <= 1.0) break;
+        }
+        inletStream.getFlowSpecies().get(i).setLiquidMoleFraction(moleFraction);
+        moleFractionSum += moleFraction; 
+      }
+      if (moleFractionSum > 0.9999 && moleFractionSum < 1.0001) {
+        break;
+      } else {
+        output.println("\nERROR: Mole fractions must add to 1.0 -- please try again!\n");
+        moleFractionSum = 0.0;
+      }
+    }
+    
+    //
+    // Are these the same as the overall mole fractions? If not, do the right thing  :) 
+    //
+    while (true) {
+      output.println("\nIs the input stream entirely in the liquid phase?");
+      output.println("  [y]es   [n]o\n");
+      scan.nextLine();
+      choice = scan.nextLine().charAt(0);
+      if (choice == 'y') {
+        for (i = 0; i < inletStream.getFlowSpecies().size(); i++) {
+          moleFraction = inletStream.getFlowSpecies().get(i).getLiquidMoleFraction();
+          inletStream.getFlowSpecies().get(i).setOverallMoleFraction(moleFraction);
+        }
+        inletStream.setVapourFraction(0.0);
+        break;
+      }
+      if (choice == 'n') {
+        output.println("\nFor each species in the inlet stream, please set the vapour mole fractions: \n");
+        moleFraction = -1.0;
+        moleFractionSum = 0.0;
+        while (true) {
+          for (i = 0; i < inletStream.getFlowSpecies().size(); i++) {
+            while (true) {
+              output.println("  Mole fraction of " + inletStream.getFlowSpecies().get(i).getSpeciesName() + ": ");
+              moleFraction = scan.nextDouble();
+              if (moleFraction >= 0.0 && moleFraction <= 1.0) break;
+            }
+            inletStream.getFlowSpecies().get(i).setVapourMoleFraction(moleFraction);
+            moleFractionSum += moleFraction; 
+          }
+          if (moleFractionSum > 0.9999 && moleFractionSum < 1.0001) {
+            break;
+          } else {
+            output.println("\nERROR: Mole fractions must add to 1.0 -- please try again!\n");
+            moleFractionSum = 0.0;
+          }
+        }
+        while (true) {
+          output.println("\nWhat fraction of the feed is in the vapour phase?");
+          moleFraction = scan.nextDouble();
+          if (moleFraction >= 0.0 && moleFraction <= 1.0) break;
+        }
+        inletStream.setVapourFraction(moleFraction);
+        for (i = 0; i < inletStream.getFlowSpecies().size(); i++) {
+          moleFraction = inletStream.getFlowSpecies().get(i).getLiquidMoleFraction() * (1.0 - inletStream.getVapourFraction())
+            + inletStream.getFlowSpecies().get(i).getVapourMoleFraction() * inletStream.getVapourFraction();
+          inletStream.getFlowSpecies().get(i).setOverallMoleFraction(moleFraction);
+        }
+        nextString = scan.nextLine();
+      }
+      break;
+    }
+    
+    //
+    // Transfer over the overall mole fractions to the outlet stream
+    //
+    for (i = 0; i < inletStream.getFlowSpecies().size(); i++) {
+      moleFraction = inletStream.getFlowSpecies().get(i).getOverallMoleFraction();
+      outletStream.getFlowSpecies().get(i).setOverallMoleFraction(moleFraction);
+    }
+    
+    
+    //
+    // Set general stream properties (T, P, flowrate)
+    //
+    output.println("\nFor the input stream, enter the following properties if known:");
+    output.println("  Temperature (K): ");
+    while (true) {
+      nextString = scan.nextLine();
+      if (nextString.isEmpty()) {
+        nextDouble = 0.0;
+        break;
+      } else {
+        nextDouble = Double.parseDouble(nextString);
+        break;
+      }
+    }
+    inletStream.setTemperature(nextDouble);
+    output.println("  Pressure (Pa): ");
+    while (true) {
+      nextString = scan.nextLine();
+      if (nextString.isEmpty()) {
+        nextDouble = 0.0;
+        break;
+      } else {
+        nextDouble = Double.parseDouble(nextString);
+        break;
+      }
+    }
+    inletStream.setPressure(nextDouble);
+    output.println("  Molar flow rate (kg/s): ");
+    while (true) {
+      nextString = scan.nextLine();
+      if (nextString.isEmpty()) {
+        nextDouble = 0.0;
+        break;
+      } else {
+        nextDouble = Double.parseDouble(nextString);
+        break;
+      }
+    }
+    inletStream.setMolarFlowRate(nextDouble);
+    output.println("\nFor the outlet stream, enter the following properties if known:");
+    output.println("  Temperature (K): ");
+    while (true) {
+      nextString = scan.nextLine();
+      if (nextString.isEmpty()) {
+        nextDouble = 0.0;
+        break;
+      } else {
+        nextDouble = Double.parseDouble(nextString);
+        break;
+      }
+    }
+    outletStream.setTemperature(nextDouble);
+    output.println("  Pressure (Pa): ");
+    while (true) {
+      nextString = scan.nextLine();
+      if (nextString.isEmpty()) {
+        nextDouble = 0.0;
+        break;
+      } else {
+        nextDouble = Double.parseDouble(nextString);
+        break;
+      }
+    }
+    outletStream.setPressure(nextDouble);
+    outletStream.setMolarFlowRate(inletStream.getMolarFlowRate());
+    
+    //
+    // Show the summary of the stream properties, and confirm if they are okay!
+    //
+    String speciesName;
+    double liquidMoleFraction, vapourMoleFraction, overallMoleFraction;
+    output.println("\nSummary of stream properties entered:");
+    output.println("NOTE: Zero may indicate that the value has not been set, e.g. for temperatures!\n");
+    output.println("Inlet:");
+    output.println("  Temperature: " + inletStream.getTemperature());
+    output.println("  Pressure: " + inletStream.getPressure());
+    output.println("  Molar flow rate: " + inletStream.getMolarFlowRate());
+    output.println("  Total vapour fraction: " + inletStream.getVapourFraction());
+    output.println("  Species and mole fractions: ");
+    output.printf("    %15s  %5s  %5s  %5s%n", "Name", " liq ", " vap ", "total");
+    output.println("----------------------------------------");
+    for (i = 0; i < inletStream.getFlowSpecies().size(); i++) {
+      speciesName = inletStream.getFlowSpecies().get(i).getSpeciesName();
+      liquidMoleFraction = inletStream.getFlowSpecies().get(i).getLiquidMoleFraction();
+      vapourMoleFraction = inletStream.getFlowSpecies().get(i).getVapourMoleFraction();
+      overallMoleFraction = inletStream.getFlowSpecies().get(i).getOverallMoleFraction();
+      output.printf("    %15s  %.3f  %.3f  %.3f%n", speciesName, liquidMoleFraction, vapourMoleFraction, overallMoleFraction);
+    }
+    output.println("Outlet:");
+    output.println("  Temperature: " + outletStream.getTemperature());
+    output.println("  Pressure: " + outletStream.getPressure());
+    output.println("  Molar flow rate: " + outletStream.getMolarFlowRate());
+    output.println("  Total vapour fraction: " + inletStream.getVapourFraction());
+    output.println("  Species and mole fractions: ");
+    output.printf("    %15s  %5s  %5s  %5s%n", "Name", "liq", "vap", "total");
+    output.println("----------------------------------------");
+    for (i = 0; i < outletStream.getFlowSpecies().size(); i++) {
+      speciesName = outletStream.getFlowSpecies().get(i).getSpeciesName();
+      liquidMoleFraction = outletStream.getFlowSpecies().get(i).getLiquidMoleFraction();
+      vapourMoleFraction = outletStream.getFlowSpecies().get(i).getVapourMoleFraction();
+      overallMoleFraction = outletStream.getFlowSpecies().get(i).getOverallMoleFraction();
+      output.printf("    %15s  %5s  %5s  %.3f%n", speciesName, " --- ", " --- ", overallMoleFraction);
+    }
+    
+    while (true) {
+      output.println("\nAre the stream properties correct?");
+      output.println("  [y]es   [n]o");
+      choice = scan.nextLine().charAt(0);
+      if (choice == 'y') break;
+      if (choice == 'n') return false;
+      if (choice == 'q') return true;  // Exit option for testing
+    }
+    
+   
     
     scan.close();
-  
+    return true;
   }
   
   private void addSpecies(Scanner scan, PrintWriter output) {
--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@ public class Main {
   public static void main(String[] args) {
     
     ConsoleUI console = new ConsoleUI();
-    console.run(new Scanner(System.in), new PrintWriter(System.out, true));
+    boolean status = false;
+    while (status == false) status = console.run(new Scanner(System.in), new PrintWriter(System.out, true));    
     
   }
 
