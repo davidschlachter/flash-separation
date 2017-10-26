70d04bddb0619c407e8bc924b6887e448eec7810
joshuarico <32338503+joshuarico@users.noreply.github.com>
2017-10-26 19:21:51 -0400

    Ragged arrays

--- Test_Fugacity.java
+++ Test_Fugacity.java
@@ -21,12 +21,17 @@ public class Test_Fugacity extends TestCase {
     Fugacity testFugacityStream = new Fugacity(testStream);
     testFugacityStream = createTestObject();
     
-    double[][] testCriticalZValues = new double[testStream.getFlowSpecies().size()][testStream.getFlowSpecies().size()];
-    
+    double[][] testCriticalZValues;
+    int n = testStream.getFlowSpecies().size();
+    testCriticalZValues = new double[n][];
+    int i = 0;
+    for(i=0; i<n; i++) {
+      testCriticalZValues[i] = new double[i+1];
+    }
     testCriticalZValues = testFugacityStream.crossSpeciesCriticalZ();
     
     assertTrue(testCriticalZValues[0][0] > 0.247 && testCriticalZValues[0][0] < 0.251);
-    assertTrue(testCriticalZValues[0][1] > 0.254 && testCriticalZValues[0][1] < 0.258);
+    assertTrue(testCriticalZValues[1][0] > 0.254 && testCriticalZValues[1][0] < 0.258);
     assertTrue(testCriticalZValues[1][1] > 0.262 && testCriticalZValues[1][1] < 0.266);
     
   }
@@ -37,12 +42,17 @@ public class Test_Fugacity extends TestCase {
     Fugacity testFugacityStream = new Fugacity(testStream);
     testFugacityStream = createTestObject();
     
-    double[][] testCriticalVolumes = new double[testStream.getFlowSpecies().size()][testStream.getFlowSpecies().size()];
-    
+    double[][] testCriticalVolumes;
+    int n = testStream.getFlowSpecies().size();
+    testCriticalVolumes = new double[n][];
+    int i = 0;
+    for(i=0; i<n; i++) {
+      testCriticalVolumes[i] = new double[i+1];
+    }
     testCriticalVolumes = testFugacityStream.crossSpeciesCriticalVolume();
     
     assertTrue(testCriticalVolumes[0][0] > 0.000266 && testCriticalVolumes[0][0] < 0.000268);
-    assertTrue(testCriticalVolumes[0][1] > 0.000290 && testCriticalVolumes[0][1] < 0.000292);
+    assertTrue(testCriticalVolumes[1][0] > 0.000290 && testCriticalVolumes[1][0] < 0.000292);
     assertTrue(testCriticalVolumes[1][1] > 0.000315 && testCriticalVolumes[1][1] < 0.000317);
     
   }
@@ -53,12 +63,17 @@ public class Test_Fugacity extends TestCase {
     Fugacity testFugacityStream = new Fugacity(testStream);
     testFugacityStream = createTestObject();
     
-    double[][] testCriticalTemps = new double[testStream.getFlowSpecies().size()][testStream.getFlowSpecies().size()];
-    
+    double[][] testCriticalTemps;
+    int n = testStream.getFlowSpecies().size();
+    testCriticalTemps = new double[n][];
+    int i = 0;
+    for(i=0; i<n; i++) {
+      testCriticalTemps[i] = new double[i+1];
+    }
     testCriticalTemps = testFugacityStream.crossSpeciesCriticalTemperature();
     
     assertTrue(testCriticalTemps[0][0] > 535.0 && testCriticalTemps[0][0] < 536.0);
-    assertTrue(testCriticalTemps[0][1] > 562.5 && testCriticalTemps[0][1] < 563.5);
+    assertTrue(testCriticalTemps[1][0] > 562.5 && testCriticalTemps[1][0] < 563.5);
     assertTrue(testCriticalTemps[1][1] > 591.5 && testCriticalTemps[1][1] < 592.5);
     
   }
@@ -69,15 +84,20 @@ public class Test_Fugacity extends TestCase {
     Fugacity testFugacityStream = new Fugacity(testStream);
     testFugacityStream = createTestObject();
     
-    double[][] testCriticalPressures = new double[testStream.getFlowSpecies().size()][testStream.getFlowSpecies().size()];
-    
+    double[][] testCriticalPressures;
+    int n = testStream.getFlowSpecies().size();
+    testCriticalPressures = new double[n][];
+    int i = 0;
+    for(i=0; i<n; i++) {
+      testCriticalPressures[i] = new double[i+1];
+    }
     testCriticalPressures = testFugacityStream.crossSpeciesCriticalPressure();
     
     testStream.setPressure(25000);
     
     assertTrue(testCriticalPressures[0][0] > 4140000 && testCriticalPressures[0][0] < 4160000);
     assertTrue(testCriticalPressures[1][1] > 4100000 && testCriticalPressures[1][1] < 4120000);
-    assertTrue(testCriticalPressures[0][1] > 4120000 && testCriticalPressures[0][1] < 4140000);
+    assertTrue(testCriticalPressures[1][0] > 4120000 && testCriticalPressures[1][0] < 4140000);
     
   }
   
@@ -87,13 +107,18 @@ public class Test_Fugacity extends TestCase {
     Fugacity testFugacityStream = new Fugacity(testStream);
     testFugacityStream = createTestObject();
     
-    double[][] testOmegaIJ = new double[testStream.getFlowSpecies().size()][testStream.getFlowSpecies().size()];
-    
+    double[][] testOmegaIJ;
+    int n = testStream.getFlowSpecies().size();
+    testOmegaIJ = new double[n][];
+    int i = 0;
+    for(i=0; i<n; i++) {
+      testOmegaIJ[i] = new double[i+1];
+    }
     testOmegaIJ = testFugacityStream.omegaIJ();
     
     assertTrue(testOmegaIJ[0][0] > 0.321 && testOmegaIJ[0][0] < 0.325);
     assertTrue(testOmegaIJ[1][1] > 0.260 && testOmegaIJ[1][1] < 0.264);
-    assertTrue(testOmegaIJ[0][1] > 0.291 && testOmegaIJ[0][1] < 0.295);
+    assertTrue(testOmegaIJ[1][0] > 0.291 && testOmegaIJ[1][0] < 0.295);
     
   }
   
@@ -103,14 +128,12 @@ public class Test_Fugacity extends TestCase {
     
     testFugacityStream.getFlowStream().setTemperature(323.15);
     
-    double[][] testBValues = new double[testFugacityStream.getFlowStream().getFlowSpecies().size()]
-      [testFugacityStream.getFlowStream().getFlowSpecies().size()];
-    
+    double[][] testBValues;
     testBValues = testFugacityStream.bValues();
     
     assertTrue(testBValues[0][0] > -0.001380 && testBValues[0][0] < -0.001370);
     assertTrue(testBValues[1][1] > -0.001875 && testBValues[1][1] < -0.001870);
-    assertTrue(testBValues[0][1] > -0.001615 && testBValues[0][1] < -0.001608);
+    assertTrue(testBValues[1][0] > -0.001615 && testBValues[1][0] < -0.001608);
     
   }
   
@@ -122,7 +145,6 @@ public class Test_Fugacity extends TestCase {
     testFugacityStream.getFlowStream().setPressure(25000.0);
     
     double[] results = new double[testFugacityStream.getFlowStream().getFlowSpecies().size()];
-    
     results = testFugacityStream.fugacityCoefficients();
     
     assertTrue(results[0] < 0.989 && results[0] > 0.985);
