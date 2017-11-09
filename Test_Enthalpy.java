92cb228cdfa7ad6cc05971d1f0f076107c629ecb
David Schlachter <david+gh@schlachter.ca>
2017-11-09 17:27:19 -0500

    Add test for pure species enthalpy change

--- Test_Enthalpy.java
+++ Test_Enthalpy.java
@@ -1,4 +1,6 @@
 import junit.framework.TestCase;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test_Enthalpy extends TestCase {
   
@@ -43,7 +45,36 @@ public class Test_Enthalpy extends TestCase {
     double theEnthalpy = enthalpy.testFunction(360.0);
     
     assertTrue("Enthalpy.testFunction()", theEnthalpy > 29691.5 && theEnthalpy < 29691.7);
+  // Test a pure species enthalpy calculation, reference is from the steam tables
+  public void testPureSpeciesEnthalpyCalculation() {
+    
+    List<FlowSpecies> presetSpecies = PresetSpecies.get();
+    
+    FlowStream inletStream = new FlowStream();
+    FlowStream outletStream = new FlowStream();
+    
+    inletStream.addFlowSpecies(new FlowSpecies(presetSpecies.get(4))); // Water
+    inletStream.getFlowSpecies().get(0).setOverallMoleFraction(1.0);
+    inletStream.getFlowSpecies().get(0).setVapourMoleFraction(1.0);
+    inletStream.setMolarFlowRate(1.0); // 1 mol/s = 3.6 kgmol/h
+    inletStream.setTemperature(125.0 + 273.15);
+    inletStream.setPressure(101325.0);
+    inletStream.setVapourFraction(1.0);
+    
+    outletStream.addFlowSpecies(new FlowSpecies(presetSpecies.get(4))); // Water
+    outletStream.getFlowSpecies().get(0).setOverallMoleFraction(1.0);
+    outletStream.getFlowSpecies().get(0).setVapourMoleFraction(1.0);
+    outletStream.setMolarFlowRate(1.0);
+    outletStream.setTemperature(150.0 + 273.15);
+    outletStream.setPressure(101325.0);
+    outletStream.setVapourFraction(1.0);
+    
+    Enthalpy enthalpy = new Enthalpy(inletStream, outletStream);
+    double theEnthalpy = enthalpy.testFunction(outletStream.getTemperature());
+    
+    // Note that real change is closer to 890 J/mol, but the correlation is a bit off
+    // in this range
+    assertTrue(theEnthalpy > 859.0 && theEnthalpy < 861.0);
   }
- 
   
 }
