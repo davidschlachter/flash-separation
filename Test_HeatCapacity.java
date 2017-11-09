1dc95e2d24d2f5eb5d99b456474fb08cdf41ccdc
David Schlachter <david+gh@schlachter.ca>
2017-11-09 17:27:40 -0500

    Add test for water vapour heat capacity integration

--- Test_HeatCapacity.java
+++ Test_HeatCapacity.java
@@ -1,4 +1,6 @@
 import junit.framework.TestCase;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test_HeatCapacity extends TestCase {
   
@@ -12,7 +14,7 @@ public class Test_HeatCapacity extends TestCase {
     
     testSpecies.setLiquidHeatCapacityConstants(heatCapacityA, heatCapacityB, heatCapacityC, heatCapacityD);
     
-    double integral = HeatCapacity.integrate(testSpecies, 295.0, 333.0, "liquid");
+    double integral = HeatCapacity.integrate(testSpecies, 295.0, 333.0, "liquid") * 8.3145;
     
     assertTrue("HeatCapacity.integrate()", integral > 4460.0 && integral < 4470.0);
   }
@@ -27,9 +29,21 @@ public class Test_HeatCapacity extends TestCase {
     
     testSpecies.setVapourHeatCapacityConstants(heatCapacityA, heatCapacityB, heatCapacityC, heatCapacityD);
     
-    double integral = HeatCapacity.integrate(testSpecies, 295.0, 333.0, "vapour");
+    double integral = HeatCapacity.integrate(testSpecies, 295.0, 333.0, "vapour") * 8.3145;
     
     assertTrue("HeatCapacity.integrate()", integral > 4460.0 && integral < 4470.0);
+  }
+  
+  // Test integrated heat capacity of water vapour. Expected values are from steam tables.
+  public void testWaterVapourIntegrate() {
+    List<FlowSpecies> presetSpecies = PresetSpecies.get();
+    Species water = new FlowSpecies(presetSpecies.get(4)); // Water
+    
+    double integral = HeatCapacity.integrate(water, 125.0+273.15, 150.0+273.15, "vapour");
+    
+    // Actual value is about 899 J/mol, but integrated correlation expected to give 
+    // 859.982 J/mol (calculated with WolframAlpha)
+    assertTrue("HeatCapacity.integrate()", integral > 859.0 && integral < 861.0);
   }
   
 }
