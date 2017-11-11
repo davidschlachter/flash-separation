9a4e22ead9abf76493ba92164e27dd9615f4d019
Stephlanctott <32338538+Stephlanctott@users.noreply.github.com>
2017-11-11 14:20:42 -0500

    make a test for liquid to vapour entalphy (ref #24 )

--- Test_Enthalpy.java
+++ Test_Enthalpy.java
@@ -71,12 +71,41 @@ public class Test_Enthalpy extends TestCase {
     Enthalpy enthalpy = new Enthalpy(inletStream, outletStream);
     double theEnthalpy = enthalpy.testFunction(outletStream.getTemperature());
     
-    System.out.println("Water enthalpy is: " + theEnthalpy);
     
-    // Note that real change is closer to 890 J/mol, but the correlation is a bit off
-    // in this range
     assertTrue(theEnthalpy > 4503.0 && theEnthalpy < 4540.0);
     
+  }
+ 
+  public void testPureSpeciesPhaseChangeEnthalpyCalculation()
+  {
+    List<FlowSpecies> presetSpecies = PresetSpecies.get();
+    
+    FlowStream inletStream = new FlowStream();
+    FlowStream outletStream = new FlowStream();
+    
+    inletStream.addFlowSpecies(new FlowSpecies(presetSpecies.get(4))); // Water
+    inletStream.getFlowSpecies().get(0).setOverallMoleFraction(1.0);
+    inletStream.getFlowSpecies().get(0).setLiquidMoleFraction(1.0);
+    inletStream.setMolarFlowRate(1.0); // 1 mol/s = 3.6 kgmol/h
+    inletStream.setTemperature(20.0 + 273.15);
+    inletStream.setPressure(101325.0);
+    inletStream.setVapourFraction(0.0);
+    
+    outletStream.addFlowSpecies(new FlowSpecies(presetSpecies.get(4))); // Water
+    outletStream.getFlowSpecies().get(0).setOverallMoleFraction(1.0);
+    outletStream.getFlowSpecies().get(0).setVapourMoleFraction(1.0);
+    outletStream.setMolarFlowRate(1.0);
+    outletStream.setTemperature(150.0 + 273.15);
+    outletStream.setPressure(101325.0);
+    outletStream.setVapourFraction(1.0);
+    
+    Enthalpy enthalpy = new Enthalpy(inletStream, outletStream);
+    double theEnthalpy = enthalpy.testFunction(outletStream.getTemperature());
+    
+    System.out.println("Water enthalpy from liquid to vapour is: " + theEnthalpy);
+    
+    
+    assertTrue(theEnthalpy > 47464.0 && theEnthalpy < 49401.0);
   }
   
 }
