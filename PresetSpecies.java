2f5cfb3527ef498897da24cf437032758b17b881
David Schlachter <david+gh@schlachter.ca>
2017-11-11 15:19:18 -0500

    Add heat of vapourization for all presetSpecies

--- PresetSpecies.java
+++ PresetSpecies.java
@@ -22,7 +22,8 @@ public class PresetSpecies {
     ethaneAntoine.add(new AntoineCoefficients(9.512776612, 791.3, -6.422, 91.33, 144.13)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C74840&Mask=4&Type=ANTOINE#ANTOINE
     ethaneAntoine.add(new AntoineCoefficients(8.944066612, 659.739, -16.719, 135.74, 199.91)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C74840&Mask=4&Type=ANTOINE#ANTOINE
     ethane.setAntoineConstants(ethaneAntoine);
-    ethane.setCriticalTemperature (305.3);
+    ethane.setHeatOfVapourization(15500.3); // Average of http://webbook.nist.gov/cgi/cbook.cgi?ID=C74840&Mask=4
+    ethane.setCriticalTemperature(305.3);
     flowSpecies.add(ethane);
     
     FlowSpecies pentane = new FlowSpecies();
@@ -30,7 +31,8 @@ public class PresetSpecies {
     pentane.setVapourHeatCapacityConstants(20.4868289472, 0.3770690663898, -0.0001173253422378, 0.0); // Smith & Van Ness, Appendix C, Table C1. Corrected to return C_p instead of C_p / R
     pentane.setLiquidHeatCapacityConstants(168.6, 0.0, 0.0, 0.0); //http://webbook.nist.gov/cgi/cbook.cgi?ID=C109660&Mask=7#Thermo-Condensed
     pentane.setAntoineConstants(new AntoineCoefficients(8.994916612, 1070.617, -40.454, 268.6, 341.37)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C109660&Mask=4&Type=ANTOINE&Plot=on
-    pentane.setCriticalTemperature (469.6);
+    pentane.setHeatOfVapourization(27623.75); // Average of http://webbook.nist.gov/cgi/cbook.cgi?ID=C109660&Mask=4
+    pentane.setCriticalTemperature(469.6);
     flowSpecies.add(pentane);
     
     FlowSpecies hexane = new FlowSpecies();
@@ -41,7 +43,8 @@ public class PresetSpecies {
     hexaneAntoine.add(new AntoineCoefficients(8.461756612, 1044.038, -53.896, 117.7, 264.93)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C110543&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
     hexaneAntoine.add(new AntoineCoefficients(9.008376612, 1171.53, -48.784, 286.18, 342.69)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C110543&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
     hexane.setAntoineConstants(hexaneAntoine);
-    hexane.setCriticalTemperature (507.6);
+    hexane.setHeatOfVapourization(30927.77778); // Average of http://webbook.nist.gov/cgi/cbook.cgi?ID=C110543&Mask=4
+    hexane.setCriticalTemperature(507.6);
     flowSpecies.add(hexane);
     
     FlowSpecies cyclohexane = new FlowSpecies();
@@ -54,7 +57,8 @@ public class PresetSpecies {
     cyclohexaneAntoine.add(new AntoineCoefficients(8.176966612, 780.637, -107.29, 315.7, 353.9)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C110827&Mask=4&Type=ANTOINE&Plot=on
     cyclohexaneAntoine.add(new AntoineCoefficients(8.975596612, 1203.562, -50.287, 293.06, 354.73)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C110827&Mask=4&Type=ANTOINE&Plot=on
     cyclohexane.setAntoineConstants(cyclohexaneAntoine);
-    cyclohexane.setCriticalTemperature (554.0);
+    cyclohexane.setHeatOfVapourization(31690.4); // Average of http://webbook.nist.gov/cgi/cbook.cgi?ID=C110827&Mask=4
+    cyclohexane.setCriticalTemperature(554.0);
     flowSpecies.add(cyclohexane);
     
     FlowSpecies water = new FlowSpecies();
@@ -70,7 +74,7 @@ public class PresetSpecies {
     waterAntoine.add(new AntoineCoefficients(11.21534661, 2354.731, 7.559, 293, 343)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C7732185&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
     waterAntoine.add(new AntoineCoefficients(9.660016612, 1435.264, -64.848, 255.9, 373.2)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C7732185&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
     water.setAntoineConstants(waterAntoine);
-    water.setHeatOfVapourization(40660.0);
+    water.setHeatOfVapourization(40714.5); // http://gchem.cm.utexas.edu/data/section2.php?target=heat-transition.php
     water.setCriticalTemperature(647.0);
     flowSpecies.add(water);
     
@@ -82,7 +86,8 @@ public class PresetSpecies {
     nitrogenAntoine.add(new AntoineCoefficients(8.741916612, 264.651, -6.788, 78.0, 126)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C7727379&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
     nitrogenAntoine.add(new AntoineCoefficients(8.643636612, 257.877, -6.344, 63.14, 78.0)); // http://webbook.nist.gov/cgi/cbook.cgi?ID=C7727379&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
     nitrogen.setAntoineConstants(nitrogenAntoine);
-    nitrogen.setCriticalTemperature (126.2);
+    nitrogen.setHeatOfVapourization(5850.0); // Average of http://webbook.nist.gov/cgi/cbook.cgi?ID=C7727379&Mask=4
+    nitrogen.setCriticalTemperature(126.2);
     flowSpecies.add(nitrogen);
     
     return flowSpecies;
