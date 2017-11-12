d6c572ef9fe84d0df5599e7ce9955218a665019d
joshuarico <32338503+joshuarico@users.noreply.github.com>
2017-11-12 01:23:26 -0500

    Added tests for RiddersMethod and NewtonRaphson

---
+++ Test_NewtonRaphson.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+public class Test_NewtonRaphson extends TestCase {
+  
+  public void testCalc() {
+    double[] coeffs = {15, 10, -2};
+    Polynomial testQuadratic = new Polynomial(coeffs);
+    double root1 = NewtonRaphson.calc(testQuadratic, 0, 0.0001);
+    double root2 = NewtonRaphson.calc(testQuadratic, 5, 0.0001);
+    assertTrue("RiddersMethod.calc()", root1 > -1.2081 && root1 < -1.2080);
+    assertTrue("RiddersMethod.calc()", root2 > 6.2080 && root2 < 6.2081);
+  }
+}
--- Test_RootFinder.java
+++ Test_RootFinder.java
@@ -6,7 +6,10 @@ public class Test_RootFinder extends TestCase {
     double[] coeffs = {15, 10, -2};
     Polynomial testQuadratic = new Polynomial(coeffs);
     double[] bounds1 = RootFinder.getBounds(testQuadratic, 0, 1);
-    assertTrue("RootFinder.lowerBound", bounds1[0] > -2.1 && bounds1[0] < -1.9);
-    assertTrue("RootFinder.upperBound", bounds1[1] > 1.9 && bounds1[1] < 2.1);
+    double[] bounds2 = RootFinder.getBounds(testQuadratic, 5, 1);
+    assertTrue("RootFinder.lowerBound1", bounds1[0] > -2.1 && bounds1[0] < -1.9);
+    assertTrue("RootFinder.upperBound1", bounds1[1] > 1.9 && bounds1[1] < 2.1);
+    assertTrue("RootFinder.lowerBound2", bounds2[0] > 3.9 && bounds2[0] < 4.1);
+    assertTrue("RootFinder.upperBound2", bounds2[1] > 6.9 && bounds2[1] < 7.1);
   }
 }
