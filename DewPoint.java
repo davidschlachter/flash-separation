7d58a9e6ae8c5d992196fd053e5cc49cf4e1daf2
David Schlachter <david+gh@schlachter.ca>
2017-11-16 16:58:58 -0500

    Continue root finding forward if an asymptote is found

--- DewPoint.java
+++ DewPoint.java
@@ -30,9 +30,17 @@ public class DewPoint implements Function {
     }
     
     // Calculate the dew point
-    double[] bounds = RootFinder.getBounds(this, flowStream.getTemperature(), 5.0);
-    double accuracy = 0.0001;
-    return RiddersMethod.calc(this, bounds[0], bounds[1], accuracy);
+    double[] bounds = RootFinder.getBounds(this, flowStream.getTemperature(), 1.0);
+    double result =  RiddersMethod.calc(this, bounds[0], bounds[1], 0.001);
+    i = 0;
+    while (Double.isNaN(result)) {
+      bounds = RootFinder.getBounds(this, bounds[1], 1.0, true);
+      result =  RiddersMethod.calc(this, bounds[0], bounds[1], 0.001);
+      if (i > 10) break;
+    }
+    return result;
+    
+    
   }
   
   
