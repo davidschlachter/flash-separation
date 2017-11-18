5a977d321816b4d07af59854c471b408c15b4473
nickboileau <32338703+nickboileau@users.noreply.github.com>
2017-11-18 07:21:40 -0500

    added individual component properties for calculation Peng Robinson EOS

--- PengRobinson.java
+++ PengRobinson.java
@@ -1,5 +1,58 @@
 public class PengRobinson{
-
-
+  
+  private FlowStream flowStream;
+  
+  //probably still needs some sort of constructor
+  
+  public void kappaI(){
+    int n = flowStream.getFlowSpecies().size();
+    double result = 0.0;
+    
+    for(int i=0; i<n; i++){
+      double omega = flowStream.getFlowSpecies().get(i).getAcentricFactor();
+      result = 0.37464+1.54226*omega-0.26992*Math.pow(omega, 2.);
+      flowStream.getFlowSpecies().get(i).setKappa(result);
+    }
+  }
+  
+  public void alphaI(){
+    int n = flowStream.getFlowSpecies().size();
+    double result = 0.0;
+    double temperature = flowStream.getTemperature();
+    
+    for(int i=0; i<n; i++){
+      double criticalTemp = flowStream.getFlowSpecies().get(i).getCriticalTemperature();
+      double kappa = flowStream.getFlowSpecies().get(i).getKappa();
+      result=Math.pow((1+kappa*(1-Math.pow((temperature/criticalTemp),0.5))),2.);
+      flowStream.getFlowSpecies().get(i).setAlpha(result);
+    }
+  }
+  
+  public void individualA(){
+    double r = 8.3145;
+    int n = flowStream.getFlowSpecies().size();
+    double result = 0.0;
+    
+    for(int i=0; i<n; i++){
+      double criticalTemp = flowStream.getFlowSpecies().get(i).getCriticalTemperature();
+      double criticalPressure = flowStream.getFlowSpecies().get(i).getCriticalPressure();
+      double alpha = flowStream.getFlowSpecies().get(i).getAlpha();
+      result = (0.45724*Math.pow((r*criticalTemp),2)*alpha)/criticalPressure;
+      flowStream.getFlowSpecies().get(i).setAI(result);
+    }
+  }
+  
+  public void individualB(){
+    double r = 8.3145;
+    int n = flowStream.getFlowSpecies().size();
+    double result = 0.0;
+    
+    for(int i=0; i<n; i++){
+      criticalPressure = flowStream.getFlowSpecies().get(i).getCriticalPressure;
+      criticalTemp = flowStream.getFlowSpecies().get(i).getCriticalTemperature();
+      result = (0.0778*r*criticalTemp)/criticalPressure;
+      flowStream.getFlowSpecies().get(i).setBI(result);
+    }
+  }
   
 }
