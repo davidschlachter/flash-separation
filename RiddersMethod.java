92a5261b4864a431a39fabe18aafd859b2410e93
David Schlachter <david+gh@schlachter.ca>
2017-11-21 12:44:28 -0500

    Add verbose Ridders' method

--- RiddersMethod.java
+++ RiddersMethod.java
@@ -1,7 +1,15 @@
 public class RiddersMethod extends RootFinder {
   
-    // Static method to find the root of a Function's testFunction method
+  // Static method to find the root of a Function's testFunction method
   public static double calc(Function func, double x1, double x2, double xacc) {
+    // Run Ridders' method in non-verbose mode
+    return RiddersMethod.calc(func, x1, x2, xacc, false);
+  }
+  
+  
+  
+// Verbose version!
+  public static double calc(Function func, double x1, double x2, double xacc, boolean verbose) {
     double xL, xU, xM, xR_old, xR, error;
     xL = x1;
     xU = x2;
@@ -16,6 +24,9 @@ public class RiddersMethod extends RootFinder {
       fL = func.testFunction(xL);
       fU = func.testFunction(xU);
       fM = func.testFunction(xM);
+      if (verbose == true) {
+        System.out.println("Ridders Method: xL="+xL+" f(xL)="+fL+", xM="+xM+" f(xM)="+fM+", xU="+xU+" f(xU)="+fU);
+      }
       xR = xM + (xM - xL)*(Math.signum(fL - fU)*fM)/(Math.sqrt(fM*fM - fL*fU));
       fR = func.testFunction(xR);
       if (Double.isNaN(xR)) {
@@ -46,7 +57,6 @@ public class RiddersMethod extends RootFinder {
       if((j > 0) && (error < xacc)) return xR;
     }
     
-    System.out.println("WARNING: Exceeded maximum number of iterations!");
     return xR;
   }
 }
