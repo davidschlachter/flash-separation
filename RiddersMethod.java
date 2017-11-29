635da1fc26c511ab6acf4037aa5a9c8d8645abdc
David Schlachter <david+gh@schlachter.ca>
2017-11-29 15:44:48 -0500

    Make Ridders Method quiet by default

--- RiddersMethod.java
+++ RiddersMethod.java
@@ -70,11 +70,11 @@ public class RiddersMethod extends RootFinder {
       }
       error = Math.abs(xR - xR_old)/xR;
       if((j > 0) && (error < xacc)) {
-        System.out.println("Root was found to be "+xR+" evaluated at "+fR);
+        if (verbose == true) System.out.println("Root was found to be "+xR+" evaluated at "+fR);
         return xR;
       }
     }
-    System.out.println("Root was found to be "+xR+" evaluated at "+func.testFunction(xR));
+    if (verbose == true) System.out.println("Root was found to be "+xR+" evaluated at "+func.testFunction(xR));
     return xR;
   }
 }
